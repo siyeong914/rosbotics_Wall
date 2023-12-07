dac80ea6452313aab6d0ac15cc053258166c9937
siyeong <syyoung0914@naver.com>
2023-12-08 06:34:18 +0900

    add first code

---
+++ code.cpp
@@ -0,0 +1,20 @@
+  void subscribe_scan(const sensor_msgs::msg::LaserScan::SharedPtr scan)
+  {
+    geometry_msgs::msg::Twist vel;
+    if (scan->ranges[0] < 0.3)
+    {
+      vel.linear.x = 0.;
+      vel.angular.z = 0.;
+    }
+    else
+    {
+      vel.linear.x = 0.1;
+      vel.angular.z = 0.;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("self_drive"),
+                "step=%d, range=%1.2f, linear=%1.2f, angular=%1.2f", step_, scan->ranges[0],
+                vel.linear.x, vel.angular.z);
+    pose_pub_->publish(vel);
+    step_++;
+  }
+};
