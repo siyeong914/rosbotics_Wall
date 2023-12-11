9f16529cbc7436040e690a0178160f77bd9a9df1
siyeong <syyoung0914@naver.com>
2023-12-12 00:02:43 +0900

    final code

--- code.cpp
+++ code.cpp
@@ -76,19 +76,19 @@ public:
 
   int detectObstacleType(const sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
-    if ((scan->ranges[0] > 0.01 && scan->ranges[0] <= 0.22) || (scan->ranges[20] > 0.01 && scan->ranges[20] <= 0.22))
+    if ((scan->ranges[0] > 0.025 && scan->ranges[0] <= 0.235) || (scan->ranges[20] > 0.025 && scan->ranges[20] <= 0.235))
     {
       return 1;
     }
-    else if((scan->ranges[30] > 0.03 && scan->ranges[30] <= 0.22) || (scan->ranges[50] > 0.03 && scan->ranges[50] <= 0.22))
+    else if((scan->ranges[30] > 0.035 && scan->ranges[30] <= 0.235) || (scan->ranges[50] > 0.035 && scan->ranges[50] <= 0.235))
     {
       return 2;
     }
-    else if((scan->ranges[60] > 0.01 && scan->ranges[60] <= 0.22)|| (scan->ranges[80] > 0.01 && scan->ranges[80] <= 0.22))
+    else if((scan->ranges[60] > 0.025 && scan->ranges[60] <= 0.235)|| (scan->ranges[80] > 0.025 && scan->ranges[80] <= 0.235))
     {
       return 3;
     }
-    else if(scan->ranges[90] > 0.01 && scan->ranges[90] <= 0.17)
+    else if(scan->ranges[90] > 0.025 && scan->ranges[90] <= 0.185)
     {
        return 4;
     }
